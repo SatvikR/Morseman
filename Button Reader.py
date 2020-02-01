063a62f6d88ab63ea8d100486e9fecfedd428c80
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-02-01 02:07:42 +0000

    Add files via upload
    WERE DONE

--- Button Reader.py
+++ Button Reader.py
@@ -1,9 +1,7 @@
 from gpiozero import Button
-from subprocess import check_call
-from signal import pause
-import os
 import time
 import _thread as thr
+import turtle
 
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
    'C':'-.-.', 'D':'-..', 'E':'.',
@@ -19,43 +17,82 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
    '7':'--...', '8':'---..', '9':'----.',
    '0':'-----', ', ':'--..--', '.':'.-.-.-',
    '?':'..--..', '/':'-..-.', '-':'-....-',
-   '(':'-.--.', ')':'-.--.-'
+   '(':'-.--.', ')':'-.--.-', " ":" "
 }
+T = turtle.Turtle()
+turtle.setup(960, 1080)
+turtle.hideturtle()
+turtle.write("Start Tapping! Click When Ready!", font=("Arial", 30), align="center")
+T.hideturtle()
 
 current_letter = ""
 current_word = []
 tick = 0
 button = Button(21) # The button
 cool_lock = thr.allocate_lock()
+total_message = ""
+currentsentence = ""
+current_buffer = []
 
-
-def decodeAlexandre(message):
+def decode(message):
+    global currentsentence
     for i in message:
         if i in list(MORSE_CODE_DICT.values()):
             for K in MORSE_CODE_DICT:
                 if MORSE_CODE_DICT[K] == i:
-                    print(K, end = "")
-    print('')
+                    currentsentence = currentsentence + K
+def dump(x, y): #DUMPS MESSAGE
+    global tick
+    global current_buffer
+    global current_word
+    global current_letter
+    global currentsentence
+    global cool_lock
+    global T
+    
+    #print("I HAPPENED")
+    print(current_buffer)
+    with cool_lock:
+        current_word.append(current_letter)
+        current_buffer.append(current_word)
+        tick = 0 
+        for word in current_buffer:
+            decode(word)
+        T.clear()
+        turtle.clear()
+        T.write(currentsentence, font=("Arial", 60), align="center")
+        currentsentence = ""
+        current_buffer = []
+        current_word = []
+        current_letter = ""
+        
 
 def ditordot():
     global current_letter
     global tick
     global cool_lock
     global current_word
+    global currentsentence
+    global current_buffer
     with cool_lock:
-        if tick>= 7:
-            tick = 0
-            print("  ", end = "")
-            current_word.append(current_letter)
-            print(current_word)
-            decodeAlexandre(current_word)
-            current_word = []
-            current_letter = ""
-        if tick >= 3 and tick <= 7:
+        if tick > 7:
             tick = 0
             print(" ", end = "")
             current_word.append(current_letter)
+            current_buffer.append(current_word)
+            #print("WORD", current_word)
+            #print("BUFFER", current_buffer)
             current_letter = ""
+            current_word = []
+            
+        if tick >= 3 and tick <= 7:
+            tick = 0
+            #print(current_word)
+            #print(current_letter)
+            current_word.append(current_letter)
+            current_letter = ""
+            print(" ", end = "")
+            
     ditlength = 0.15#if the button press is less than this length, it will be a dit, else a dot
     badlength = 0.009
     global button
@@ -74,15 +111,22 @@ def ditordot():
         tick = 0
 
 button.when_pressed = ditordot
-
+#you cannot hear the bass in metal
 
 def threadfunction():
     global tick
-    print("HERE")
+    print("START TAPPING!!")
     while True:
         time.sleep(0.15)
         with cool_lock:
             tick += 1
             
-            
-thr.start_new_thread(threadfunction, ()) 
+
+                
+thr.start_new_thread(threadfunction, ())
+#thr.start_new_thread(readerboard, ())
+turtle.onscreenclick(dump)
+turtle.mainloop()
+while True:
+    pass
+
